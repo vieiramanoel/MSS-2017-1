46f6bc291652757e32a9eb1fcf6eecf0e08878f3
Vieira <vieiranetoc@gmail.com>
2017-04-07 12:30:26 -0300

    restarted second project

--- semaforo/src/main.cpp
+++ semaforo/src/main.cpp
@@ -4,11 +4,12 @@
  *  Created on: 7 de abr de 2017
  *      Author: vieira
  */
+
+
 #include <systemc.h>
 
-int sc_main(int argc, char* argv[]){
-
+int sc_main(int argc,  char* argv[]){
+	std::cout << "hello world" << std::endl;
 	return 0;
 }
-
 
